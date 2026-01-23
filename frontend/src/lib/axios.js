1c82d69015515af9751d228c13d01378bd1117e0
Nelly6002 <etopnelson06@gmail.com>
2026-01-23 16:21:47 +0100

    iphones fixed

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://fullstack-chat-app-master-xydu.onrender.com/api",
+  baseURL: "/api",
   withCredentials: true,
 });
